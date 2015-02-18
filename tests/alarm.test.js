b7c365f0d8c3f5e9a0f24ebe62f31bad603de51e
ivmartel <ivmartel@gmail.com>
2015-02-18 22:57:37 +0100

    More jshint fix.

--- tests/alarm.test.js
+++ tests/alarm.test.js
@@ -2,11 +2,12 @@
  * Tests for the 'alarm.js' file.
  */
 // Do not warn if these variables were not defined before.
-/* global module, test, equal, ok */
+/* global module, test, equal */
 module("alarm");
 
 test("Test clock.", function() {
     // basic clock
     var clock = new wal.Clock();
+    clock.start();
     equal(1, 1, "Stupid test.");
 });
