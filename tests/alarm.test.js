498e32ff922619ecaca1b1602e8c1e9125bf16b6
ivmartel <ivmartel@gmail.com>
2015-02-18 23:19:15 +0100

    Updated test.

--- tests/alarm.test.js
+++ tests/alarm.test.js
@@ -2,12 +2,13 @@
  * Tests for the 'alarm.js' file.
  */
 // Do not warn if these variables were not defined before.
-/* global module, test, equal */
+/* global module, test, deepEqual */
 module("alarm");
 
-test("Test clock.", function() {
-    // basic clock
-    var clock = new wal.Clock();
-    clock.start();
-    equal(1, 1, "Stupid test.");
+test("Test getQueryParams.", function() {
+    // query string
+    var qs = "?a=0&b=1";
+    var params = wal.getQueryParams(qs);
+    var theo = { "a": "0", "b": "1" };
+    deepEqual(params, theo, "getQueryParams test.");
 });
