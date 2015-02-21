67fed1fae573e39fbeded62ea7bf54ba1e188cec
Travis <travis@travis-ci.org>
2015-02-21 14:38:09 +0000

    Travis build 18 pushed to gh-pages

--- demo/trunk/wal.js
+++ demo/trunk/wal.js
@@ -1,44 +1,67 @@
-// Web ALarm
+// namespace
 var wal = wal || {};
 
-// append a zero to one digit time.
-wal.prettyTime = function (t) {
-    return ( t < 10 ) ? "0" + t : t;
-};
-// clear a div from its content
-wal.clearDiv = function (div) {
-    while ( div.firstChild ) {
-        div.removeChild( div.firstChild );
-    }
-};
-// get a Date object from a time given as: "HH:MM"
-wal.time2Date = function (time) {
-    var date = null;
-    var indexSemiCol = time.indexOf(':');
-    if ( indexSemiCol != -1 ) {
-        date = new Date();
-        date.setHours( time.substr(0,indexSemiCol) );
-        date.setMinutes( time.substr(indexSemiCol+1) );
-        date.setSeconds( 0 );
-    }
-    return date;
-};
-// get query parameters
-wal.getQueryParams = function (qs) {
-    qs = qs.split("+").join(" ");
+/**
+ * Alarm.
+ * Possible actions:
+ * - Simplest that works:
+ *   window.location = alarmUrl;
+ * - Will be blocked as pop-up:
+ *   window.open(alarmUrl, '_blank');
+ * - Needs same origin:
+ *   var iframe = document.getElementById("alarm-iframe");
+ *   iframe.src = alarmUrl;
+ */
+wal.Alarm = function (time, action)
+{
+    // Check input time.
+    this.verify = function () {
+        var now = new Date();
+        if ( time instanceof Date &&
+            time.getTime() !== 0 &&
+            time.getTime() > now.getTime() ) {
+            return true;
+        }
+        return false;
+    };
 
-    var params = {};
-    var re = /[?&]?([^=]+)=([^&]*)/g;
+    // Check the input date and call action if equal.
+    this.check = function (date) {
+        // compare input to member (ignore milliseconds)
+        if ( time && typeof time.getTime != "undefined" && time.getTime() !== 0 &&
+            Math.abs(time.getTime() - date.getTime()) < 1000 ) {
+            return action.run();
+        }
+        return false;
+    };
 
-    var tokens = re.exec(qs);
-    while ( tokens ) {
-        params[ decodeURIComponent(tokens[1]) ] =
-            decodeURIComponent(tokens[2]);
-        tokens = re.exec(qs);
-    }
-
-    return params;
+    // Show the alarm
+    this.show = function (divId) {
+        var div = document.getElementById(divId);
+        wal.clearDiv(div);
+        div.style.display = 'block';
+        var text = "Alarm set for " +
+            wal.prettyTime(time.getHours()) + "h" +
+            wal.prettyTime(time.getMinutes()) +
+            " with action to " + action.getText() + ".";
+        div.appendChild( document.createTextNode(text) );
+    };
 };
+
+// Open URL action.
+wal.OpenUrlAction = function (url)
+{
+    // Get the text description of the action.
+    this.getText = function () {
+        return "open url: " + url;
+    };
+    // Run the action.
+    this.run = function () {
+        window.location = url;
+    };
+};
+;// namespace
+var wal = wal || {};
 
 // Clock
 wal.Clock = function (divId)
@@ -103,63 +126,85 @@ wal.Clock = function (divId)
             wal.prettyTime(date.getSeconds()) ) );
     };
 };
+;// namespace
+var wal = wal || {};
 
-/**
- * Alarm.
- * Possible actions:
- * - Simplest that works:
- *   window.location = alarmUrl;
- * - Will be blocked as pop-up:
- *   window.open(alarmUrl, '_blank');
- * - Needs same origin:
- *   var iframe = document.getElementById("alarm-iframe");
- *   iframe.src = alarmUrl;
- */
-wal.Alarm = function (time, action)
-{
-    // Check input time.
-    this.verify = function () {
-        var now = new Date();
-        if ( time instanceof Date &&
-            time.getTime() !== 0 &&
-            time.getTime() > now.getTime() ) {
-            return true;
-        }
-        return false;
-    };
-
-    // Check the input date and call action if equal.
-    this.check = function (date) {
-        // compare input to member (ignore milliseconds)
-        if ( time && typeof time.getTime != "undefined" && time.getTime() !== 0 &&
-            Math.abs(time.getTime() - date.getTime()) < 1000 ) {
-            return action.run();
-        }
-        return false;
-    };
-
-    // Show the alarm
-    this.show = function (divId) {
-        var div = document.getElementById(divId);
-        wal.clearDiv(div);
-        div.style.display = 'block';
-        var text = "Alarm set for " +
-            wal.prettyTime(time.getHours()) + "h" +
-            wal.prettyTime(time.getMinutes()) +
-            " with action to " + action.getText() + ".";
-        div.appendChild( document.createTextNode(text) );
-    };
+// append a zero to one digit time.
+wal.prettyTime = function (t) {
+    return ( t < 10 ) ? "0" + t : t;
 };
 
-// Open URL action.
-wal.OpenUrlAction = function (url)
-{
-    // Get the text description of the action.
-    this.getText = function () {
-        return "open url: " + url;
-    };
-    // Run the action.
-    this.run = function () {
-        window.location = url;
-    };
+// clear a div from its content
+wal.clearDiv = function (div) {
+    while ( div.firstChild ) {
+        div.removeChild( div.firstChild );
+    }
+};
+
+// get a Date object from a time given as: "HH:MM"
+wal.time2Date = function (time) {
+    var date = null;
+    var indexSemiCol = time.indexOf(':');
+    if ( indexSemiCol != -1 ) {
+        date = new Date();
+        date.setHours( time.substr(0,indexSemiCol) );
+        date.setMinutes( time.substr(indexSemiCol+1) );
+        date.setSeconds( 0 );
+    }
+    return date;
+};
+
+// get query parameters
+wal.getQueryParams = function (qs) {
+    qs = qs.split("+").join(" ");
+
+    var params = {};
+    var re = /[?&]?([^=]+)=([^&]*)/g;
+
+    var tokens = re.exec(qs);
+    while ( tokens ) {
+        params[ decodeURIComponent(tokens[1]) ] =
+            decodeURIComponent(tokens[2]);
+        tokens = re.exec(qs);
+    }
+
+    return params;
+};
+
+// show suggestions stored in a json file.
+wal.showSuggestions = function (filename, divId) {
+    // suggestions
+    $.getJSON(filename, function (json) {
+        // check keys
+        var keys = Object.keys(json);
+        if ( keys.length === 0 ) {
+            return;
+        }
+        // html select
+        var select = document.createElement("select");
+        select.id = "alarm-suggest";
+        // first select option
+        var option = document.createElement("option");
+        option.value = "choose-one";
+        option.appendChild( document.createTextNode("Choose one...") );
+        select.appendChild(option);
+        // select options
+        for ( var i = 0; i < keys.length; ++i ) {
+            option = document.createElement("option");
+            option.value = json[keys[i]].url;
+            option.appendChild( document.createTextNode(json[keys[i]].title) );
+            select.appendChild(option);
+        }
+        // contain div
+        var div = document.getElementById(divId);
+        // select label
+        var label = document.createElement("label");
+        label.for = "alarm-suggest";
+        label.appendChild( document.createTextNode("Suggestions:") );
+        div.appendChild(label);
+        // append select
+        div.appendChild(select);
+        // trigger creation
+        $("#"+divId).trigger("create");
+    });
 };
